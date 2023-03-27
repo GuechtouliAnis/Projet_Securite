d8db8b0a753059b7409f28698fa73012f47f2e9d
GuechtouliAnis <guechtoulianiss7@gmail.com>
2023-03-27 16:04:41 +0100

    Finishing part1

--- Part1encode.py
+++ Part1encode.py
@@ -17,17 +17,20 @@ for i in sizes:
     start_time = time.time()
     generate_part1('random'+str(i)+'mb.txt', i*default)
     end_time=time.time()-start_time
+    fileE = 'random'+str(i)+'mb.txt'
+    fileO = 'random'+str(i)+'mb_encrypted.cryp'
+    start_time = time.time()
+    command = f"openssl enc -e -aes-256-cbc -in {fileE} -out {fileO} -pass pass:aniss"
+    end_time=time.time()-start_time
     files1={"Size":str(i)+'mb',"Time":end_time}
     files["child"+str(j)] =files1
-    print("child "+str(j)+" encoded succesfully with a creation time of "+str(end_time))
+    print("child "+str(j)+" encoded succesfully with an encryption time of "+str(end_time))
     j+=1
-x_values = [child_dict["Size"] for child_dict in files.values()]
+print(files)
+"""x_values = [child_dict["Size"] for child_dict in files.values()]
 y_values = [child_dict["Time"] for child_dict in files.values()]
 plt.plot(x_values, y_values, marker='o')
 plt.xlabel("Size")
 plt.ylabel("Time")
 plt.title("File Creation Time by Size")
-plt.show()
-# Run the `ls` command and print its output
-#output = subprocess.run(['ls', '-al'], capture_output=True, text=True)
-#print(output.stdout)
+plt.show()"""
